5635a5c102a56696ad8c60498bbf8e858e2266a8
HRB05 <hrb10@kent.ac.uk>
2026-02-13 19:29:33 +0000

    Seconnd Feature - And starting to develop main

--- fleet_manager.py
+++ fleet_manager.py
@@ -6,3 +6,34 @@ def init_database():
     Ids = [0,1,2,3]
     return Names, Ranks, Divs, Ids
 
+def display_menu():
+    print("-=x=-=x=-=x=- MENU -=x=-=x=-=x=-")
+    print("1) Add Members")
+    print("2) Remove Members")
+    print("3) Update Rank")
+    print("4) Display Roster")
+    print("5) Search Crew : 5")
+    print("6) Filter By Division")
+    print("7) Calculate Payroll")
+    print("8) Count Officers")
+    print("-=x=-=x=-=x=-=x=-=x==x=-=x=-=x=-=x=-=x=-")
+    print(f"{Fname} is currently logged in")
+    print("-=x=-=x=-=x=-=x=-=x==x=-=x=-=x=-=x=-=x=-")
+    while True:
+        try:
+            choice = int(input("What option do you want to select >> "))
+            if 0 <= choice <= 8:
+                break
+        except:
+            continue
+    return choice
+
+
+def main():
+    global Fname
+    Fname = input("What is your full name >> ")
+    while True:
+        init_database()
+        break
+
+main()
