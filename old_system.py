d4316862bd7bd4f2705a7b600e1e4ae7df17e927
HRB05 <hrb10@kent.ac.uk>
2026-02-13 17:30:23 +0000

    Fix 1

--- old_system.py
+++ old_system.py
@@ -25,7 +25,7 @@ def run_system_monolith():
         
         opt = input("Select option: ")
         
-        if opt = "1":  
+        if opt == "1": # Needed two equals as is checking if equal to 1
             print("Current Crew List:")
             
             for i in range(10):
